22afd6c3fe208f80f44842e8424a10eca27fe568
adarshvs1234 <va854173@gmail.com>
2024-11-12 16:20:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className='bg-zinc-800' >
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/><br/><br/>
+    <h6 className='text-center text-left decoration-black border-[1.5px] rounded-[80px] border-black  text-7xl  font-sans '>Hello.....</h6>
+
+    <h1 className=' text-center text-9xl  font-serif'>I'm A Full Stack</h1>
+    <h1 className='text-center text-9xl text-white font-serif' >Web Developer.</h1>
+
+<br/>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/> 
+{/* <img className='bg-contain bg-left ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi96edRT2AnQYfKK-AMKuMNdg6ULtzGwESdA&s'></img> */}
+
+    <h2 className='underline italic text-right  font-extrabold font-serif  text-7xl' >Adarsh v sujith</h2>
+  
+ </div>
+
+ 
+
+
+
   );
+
+
 }
 
 export default App;
