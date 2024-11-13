3331d72fd2ea7f4cd3e134a7559f6c18fe5560c3
adarshvs1234 <va854173@gmail.com>
2024-11-13 12:12:00 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
 
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    
-    <div className='bg-zinc-800' >
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/><br/><br/>
-    <h6 className='text-center text-left decoration-black border-[1.5px] rounded-[80px] border-black  text-7xl  font-sans '>Hello.....</h6>
 
-    <h1 className=' text-center text-9xl  font-serif'>I'm A Full Stack</h1>
-    <h1 className='text-center text-9xl text-white font-serif' >Web Developer.</h1>
-
+    <div className='bg-red-700 border-double border-4 border-rose-200' >
+      <br/>
+     
+<h6 className='contrast-100 transition ease-in-out delay-150 bg-stone-200 hover:-translate-y-1 hover:scale-98 hover:bg-stone-400 duration-300   text-center text-left decoration-black border-[1.5px] rounded-[80px] border-stone-100 text-7xl  font-sans  shadow-lg  shadow-white-500/50 '>Hello.....</h6>
 <br/>
-<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/> 
-{/* <img className='bg-contain bg-left ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi96edRT2AnQYfKK-AMKuMNdg6ULtzGwESdA&s'></img> */}
-
-    <h2 className='underline italic text-right  font-extrabold font-serif  text-7xl' >Adarsh v sujith</h2>
-  
- </div>
+  <h1 className='underline decoration-dashed text-center text-5xl  font-serif'>I'm A Full Stack</h1>
+  <br/>
+  <h1 className='  text-center text-6xl text-white font-serif' >Web Developer.</h1>
 
  
+{/* <img className='bg-contain bg-left ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi96edRT2AnQYfKK-AMKuMNdg6ULtzGwESdA&s'></img> */}
+<br/>
+  <h2 className='underline italic text-right  font-extrabold font-serif  text-2xl' >Adarsh v sujith</h2>
 
-
-
-  );
-
-
+</div>
+  )
 }
 
-export default App;
+
+export default App
+
+
